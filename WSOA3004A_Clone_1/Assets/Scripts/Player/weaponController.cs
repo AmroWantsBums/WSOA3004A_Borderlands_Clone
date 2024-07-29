092ce10d995200cd0975caeff2a269ac17a240ac
Amro <2592997@students.wits.ac.za>
2024-07-29 21:49:27 +0200

    Update

--- WSOA3004A_Clone_1/Assets/Scripts/Player/weaponController.cs
+++ WSOA3004A_Clone_1/Assets/Scripts/Player/weaponController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class weaponController : MonoBehaviour
 {
@@ -9,11 +10,15 @@ public class weaponController : MonoBehaviour
     public Vector2 shootDirection;
     public float bulletSpeed;
     private bool canFire = true;
+    private int Ammo;
+    private TextMeshProUGUI ammoText;
     // Start is called before the first frame update
     void Start()
     {
+        Ammo = 20;
         Weapon = GameObject.Find("Gun");
         Cursor.visible = false;
+        ammoText = GameObject.Find("AmmoTxt").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -26,7 +31,7 @@ public class weaponController : MonoBehaviour
         {
             if (Weapon.CompareTag("Explosive_Weapon"))
             {
-                //insert the type of weapon here and put in the firerate and number of bullets parameters
+                //insert the type of weapon here and put in the firerate, bullet type and number of bullets parameters
                 // eg. if (activeWeapon.name == "pistol") {
                 StartCoroutine(Fire(0.4f, 1, Bullets[0]));
                 canFire = false;
@@ -34,7 +39,7 @@ public class weaponController : MonoBehaviour
             }
             else if (Weapon.CompareTag("Incendiary_Weapon"))
             {
-                //insert the type of weapon here and put in the firerate and number of bullets parameters
+                //insert the type of weapon here and put in the firerate, bullet type and number of bullets parameters
                 // eg. if (activeWeapon.name == "pistol") {
                 StartCoroutine(Fire(0.4f, 1, Bullets[1]));
                 canFire = false;
@@ -42,26 +47,42 @@ public class weaponController : MonoBehaviour
             }
             else if (Weapon.CompareTag("Slag_Weapon"))
             {
-                //insert the type of weapon here and put in the firerate and number of bullets parameters
+                //insert the type of weapon here and put in the firerate, bullet type and number of bullets parameters
                 // eg. if (activeWeapon.name == "pistol") {
                 StartCoroutine(Fire(0.4f, 1, Bullets[2]));
                 canFire = false;
                 // }
             }
         }
+        ammoText.text = $"{Ammo}";
     }
 
     IEnumerator Fire(float fireRate, int numberOfBullets, GameObject bulletType)
     {
-        for (int i = 0; i < numberOfBullets; i++)
+        if (numberOfBullets <= Ammo)
         {
-            GameObject spawnedBullet = Instantiate(bulletType, Weapon.transform.position, Quaternion.identity);
-            Rigidbody2D bulletRb = spawnedBullet.GetComponent<Rigidbody2D>();
-            Vector2 bulletOffset = new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
-            shootDirection = (shootDirection + bulletOffset).normalized;
-            bulletRb.AddForce(shootDirection * bulletSpeed, ForceMode2D.Impulse);
+            for (int i = 0; i < numberOfBullets; i++)
+            {
+                GameObject spawnedBullet = Instantiate(bulletType, Weapon.transform.position, Quaternion.identity);
+                Rigidbody2D bulletRb = spawnedBullet.GetComponent<Rigidbody2D>();
+                Vector2 bulletOffset = new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
+                shootDirection = (shootDirection + bulletOffset).normalized;
+                bulletRb.AddForce(shootDirection * bulletSpeed, ForceMode2D.Impulse);
+                Ammo = Ammo - numberOfBullets;
+            }
+            yield return new WaitForSeconds(fireRate);
+            canFire = true;
         }
-        yield return new WaitForSeconds(fireRate);
+        else
+        {
+            StartCoroutine(reloadWeapon());
+        }        
+    }
+
+    IEnumerator reloadWeapon()
+    {
+        yield return new WaitForSeconds(2f);
+        Ammo = 20;
         canFire = true;
     }
 }
