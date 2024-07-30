3709efd21432f73ad891fdf6d7f00d645db420a1
Etsalot <ettienne.swart95@gmail.com>
2024-07-30 13:10:39 +0200

    Gun

--- WSOA3004A_Clone_1/Assets/Scripts/GunGen/WeaponGenerator.cs
+++ WSOA3004A_Clone_1/Assets/Scripts/GunGen/WeaponGenerator.cs
@@ -12,6 +12,23 @@ public class WeaponGenerator : MonoBehaviour
     public List<GameObject> handleParts;
     public List<GameObject> magazineParts;
 
+
+    public List<GameObject> Pistolbodypart;
+    public List<GameObject> Pistolbarrelpart;
+    public List<GameObject> Pistolstockpart;
+    public List<GameObject> Pistolscopepart;
+    public List<GameObject> Pistolhandlepart;
+    public List<GameObject> Pistolmagazinepart;
+
+
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +41,13 @@ public class WeaponGenerator : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)){
             GenerateWeapon(); // change to be different for each weapon must change, eg GeneratePistol
         }
+
+        if (Input.GetKeyDown(KeyCode.P)){
+            GeneratePistol();
+        }
     }
 
-    void GenerateWeapon(){
+    void GenerateWeapon(){// random 
         GameObject randomBody = GetRandomPart(bodyParts);
        GameObject insBody = Instantiate(randomBody, Vector3.zero, Quaternion.identity);
         WeaponBody wpnBody = insBody.GetComponent<WeaponBody>();
@@ -40,18 +61,39 @@ public class WeaponGenerator : MonoBehaviour
         SpawnWeaponPart(scopeParts,wpnBody.scopeSocket);
 
         SpawnWeaponPart(magazineParts,wpnBody.magazineSocket);
-
-        
-        
-        
-        
-        
         //get random body from list
         //instantiate it
     }
 
+
+    void GeneratePistol(){// random for pistol only
+        GameObject randomBody = GetPistolPart(Pistolbodypart);
+       GameObject insBody = Instantiate(randomBody, Vector3.zero, Quaternion.identity);
+        WeaponBody pistolBody = insBody.GetComponent<WeaponBody>();
+
+        SpawnPistolPart(Pistolbarrelpart,pistolBody.barrelSocket);
+
+        SpawnPistolPart(Pistolstockpart,pistolBody.stockSocket);
+
+        SpawnPistolPart(Pistolhandlepart,pistolBody.handleSocket);
+
+        SpawnPistolPart(Pistolscopepart,pistolBody.scopeSocket);
+
+        SpawnPistolPart(Pistolmagazinepart,pistolBody.magazineSocket);
+        //get random body from list
+        //instantiate it
+    }
+
+
+
     void SpawnWeaponPart(List<GameObject> parts, Transform socket){
         GameObject randomPart = GetRandomPart(parts);
+        GameObject insPart = Instantiate(randomPart, socket.transform.position, socket.transform.rotation);
+        insPart.transform.parent = socket;
+    }
+
+    void SpawnPistolPart(List<GameObject> parts, Transform socket){
+        GameObject randomPart = GetPistolPart(parts);
         GameObject insPart = Instantiate(randomPart, socket.transform.position, socket.transform.rotation);
         insPart.transform.parent = socket;
     }
@@ -60,5 +102,11 @@ public class WeaponGenerator : MonoBehaviour
     GameObject GetRandomPart(List<GameObject> partList){
         int randomNumber = Random.Range(0, partList.Count);
         return partList[randomNumber];
+    }
+
+
+    GameObject GetPistolPart(List<GameObject> pistolpartList){
+        int pistolrandomNumber = Random.Range(0, pistolpartList.Count);
+        return pistolpartList[pistolrandomNumber];
     }
 }
