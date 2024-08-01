db6b7cd657480cd321499a71e72a3057c5457cfd
Kiewiet246 <kiewiet.lombaard01@gmail.com>
2024-08-01 21:57:57 +0200

    UpdatePrefabsForCollectables

--- WSOA3004A_Clone_1/Assets/Scripts/Loot/Collectables/RelicPrefab.cs
+++ WSOA3004A_Clone_1/Assets/Scripts/Loot/Collectables/RelicPrefab.cs
@@ -6,7 +6,7 @@ public class RelicPrefab : MonoBehaviour
 {
 
     [SerializeField]
-    private float AdjustValueCommon, AdjustValueUncommon;
+    private float AdjustValueCommon = 100, AdjustValueUncommon = 10;
     [SerializeField]
     private SpawnLoot spawnLootScript;
 
@@ -19,25 +19,42 @@ public class RelicPrefab : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            //for (int i = 0; i < spawnLootScript.regularEnemy.rarityPools.Count; i++)
-            // {
-            //  if( spawnLootScript.regularEnemy.rarityPools[i].selectRarities == SpawnLoot.EnemyType.RarityPool.AvailableRarities.Common &&
-            //       spawnLootScript.regularEnemy.rarityPools[i].Weight > 0 )
-            //     {
-            //         SpawnLoot.EnemyType.RarityPool modifyPool = spawnLootScript.regularEnemy.rarityPools[i];
+            for (int i = 0; i < spawnLootScript.regularEnemy.rarityPools.Count; i++)
+            {
+                if (spawnLootScript.regularEnemy.rarityPools[i].selectRarities == SpawnLoot.EnemyType.RarityPool.AvailableRarities.Common &&
+                     spawnLootScript.regularEnemy.rarityPools[i].Weight > 0)
+                {
+                    SpawnLoot.EnemyType.RarityPool modifyPool = spawnLootScript.regularEnemy.rarityPools[i];
+                    modifyPool.Weight = spawnLootScript.regularEnemy.rarityPools[i].Weight - AdjustValueCommon;
+                    spawnLootScript.regularEnemy.rarityPools[i] = modifyPool;
 
-            //         modifyPool.Weight =  spawnLootScript.regularEnemy.rarityPools[i].Weight - AdjustValueCommon;
-            //         spawnLootScript.regularEnemy.rarityPools[i] = modifyPool;
+                }
 
-            //     }
+                if (spawnLootScript.regularEnemy.rarityPools[i].selectRarities == SpawnLoot.EnemyType.RarityPool.AvailableRarities.Uncommon &&
+                     spawnLootScript.regularEnemy.rarityPools[i].Weight > 0)
+                {
+                    SpawnLoot.EnemyType.RarityPool modifyPool = spawnLootScript.regularEnemy.rarityPools[i];
+                    modifyPool.Weight = spawnLootScript.regularEnemy.rarityPools[i].Weight - AdjustValueUncommon;
+                    spawnLootScript.regularEnemy.rarityPools[i] = modifyPool;
 
-            //  else if ()
-            //     {
-            //         SpawnLoot.EnemyType.RarityPool modifyPoolBA = spawnLootScript.badAssEnemy.rarityPools[i];
-            //         modifyPoolBA.Weight = spawnLootScript.badAssEnemy.rarityPools[i].Weight - AdjustValueUncommon;
-            //         spawnLootScript.badAssEnemy.rarityPools[i] = modifyPoolBA;
-            //     }
-            // }
+                }
+
+                if (spawnLootScript.badAssEnemy.rarityPools[i].selectRarities == SpawnLoot.EnemyType.RarityPool.AvailableRarities.Common &&
+                     spawnLootScript.badAssEnemy.rarityPools[i].Weight > 0)
+                {
+                    SpawnLoot.EnemyType.RarityPool modifyPoolBA = spawnLootScript.badAssEnemy.rarityPools[i];
+                    modifyPoolBA.Weight = spawnLootScript.badAssEnemy.rarityPools[i].Weight - AdjustValueCommon;
+                    spawnLootScript.badAssEnemy.rarityPools[i] = modifyPoolBA;
+                }
+
+                if (spawnLootScript.badAssEnemy.rarityPools[i].selectRarities == SpawnLoot.EnemyType.RarityPool.AvailableRarities.Uncommon &&
+                     spawnLootScript.badAssEnemy.rarityPools[i].Weight > 0)
+                {
+                    SpawnLoot.EnemyType.RarityPool modifyPoolBA = spawnLootScript.badAssEnemy.rarityPools[i];
+                    modifyPoolBA.Weight = spawnLootScript.badAssEnemy.rarityPools[i].Weight - AdjustValueUncommon;
+                    spawnLootScript.badAssEnemy.rarityPools[i] = modifyPoolBA;
+                }
+            }
 
             Destroy(gameObject);
         }
